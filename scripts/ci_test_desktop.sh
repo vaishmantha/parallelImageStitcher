4dbfc88a8c31c789567fd5f880cb8e2b07af5355
Adithi Rao <adigrao2012@gmail.com>
2021-05-04 02:33:09 -0400

    testing with no data folder for ghc

--- scripts/ci_test_desktop.sh
+++
@@ -1,5 +0,0 @@
-# This script will be called by CI from the root directory
-echo "run examples for desktop"
-cd ./platforms/desktop/build/bin
-./image_match img1.pgm img2.pgm
-./feature_extract img1.pgm
--- scripts/format-code.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-clang-format -i ${dir}/../**/*.cpp ${dir}/../**/*.h
-
-
