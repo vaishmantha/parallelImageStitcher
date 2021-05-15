34e848a19804c89023cf07fa755c9bb2f2213c6b
Adithi Rao <adigrao2012@gmail.com>
2021-05-15 01:28:16 -0400

    correctness fix hopefully

--- examples/image_stitching_cuda/image_stitching.cpp
+++ examples/image_stitching_cuda/image_stitching.cpp
@@ -160,7 +160,6 @@ MatrixXd computeRansac(std::list<ezsift::MatchPair> match_li){
         for(int i = 0; i < prod.cols(); i++){
             if(prod.transpose()(i, 2) == 0){
                 divide_by_zero = true;
-                break;
             }
             if(!divide_by_zero){
                 diff = (Matslice(prod.transpose(), i, 0, 1, 2)/prod.transpose()(i, 2) - Matslice(locs1, i, 0, 1, locs1.cols())).norm(); 
