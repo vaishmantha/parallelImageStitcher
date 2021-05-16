51b493c43bc4f5ea5369f0fade0bf7654fbc78d4
Adithi Rao <adigrao2012@gmail.com>
2021-05-16 12:52:56 -0400

    Debugging cuda

--- examples/image_stitching_cuda/image_stitching.cpp
+++ examples/image_stitching_cuda/image_stitching.cpp
@@ -536,11 +536,11 @@ int main(int argc, char *argv[])
         // MatrixXd newImG = MatrixXd::Constant(curr_height, curr_width, 0);
         // MatrixXd newImB = MatrixXd::Constant(curr_height, curr_width, 0);
         // MatrixXd newImA = MatrixXd::Constant(curr_height, curr_width, 0);
-        double warpPerspectiveStart = CycleTimer::currentSeconds();
-        warpPerspective(png_r[i], png_g[i], png_b[i], png_alpha[i], widths[i], heights[i], newImR, newImG, newImB, newImA, homographies[i], curr_width, curr_height);
+        //double warpPerspectiveStart = CycleTimer::currentSeconds();
+        //warpPerspective(png_r[i], png_g[i], png_b[i], png_alpha[i], widths[i], heights[i], newImR, newImG, newImB, newImA, homographies[i], curr_width, curr_height);
         // warpPerspective(png_r[i], png_g[i], png_b[i], png_alpha[i], widths[i], heights[i], &newImR, &newImG, &newImB, &newImA, homographies[i]);
-        double warpPerspectiveEnd = CycleTimer::currentSeconds();
-        std::cout << "Warp perspective time: " << warpPerspectiveEnd-warpPerspectiveStart << std::endl;
+        //double warpPerspectiveEnd = CycleTimer::currentSeconds();
+        //std::cout << "Warp perspective time: " << warpPerspectiveEnd-warpPerspectiveStart << std::endl;
 
         // #pragma omp parallel for schedule(dynamic) // DO NOT ADD BACK IN
         for(int j= 0; j<4; j++){
