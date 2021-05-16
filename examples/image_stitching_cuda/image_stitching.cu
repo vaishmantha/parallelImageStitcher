8568636161a485f77d388c9f844876badcd59804
Adithi Rao <adigrao2012@gmail.com>
2021-05-16 17:24:41 -0400

    Timing cuda

--- examples/image_stitching_cuda/image_stitching.cu
+++ examples/image_stitching_cuda/image_stitching.cu
@@ -51,14 +51,15 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
     double overallStartTime = CycleTimer::currentSeconds();
     dim3 blockDim(32, 32, 1);
     dim3 gridDim((png_width + blockDim.x - 1) / blockDim.x, ((png_height + blockDim.y - 1) / blockDim.y));
-
+    
+    double section1S = CycleTimer::currentSeconds();
     // double* H_device;
     double *H_data = H.data();
-    // printf("H data %d %d %d %d %d %d %d %d %d", H_data[0], H_data[1], H_data[2], H_data[3], H_data[4], H_data[5], H_data[6], H_data[7], H_data[8]);
-    // cudaMalloc((void **)&H_device, 3*3*sizeof(double)); //homography
-    // cudaMemcpy(H_device, H_data, 3*3*sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpyToSymbol(homography, H_data, sizeof(double)*9);
-
+    double section1E = CycleTimer::currentSeconds();
+    std::cout << "Section1 time " << section4E-section4S << std::endl;
+    
+    double section2S = CycleTimer::currentSeconds();
     unsigned char* png_r_device;
     unsigned char* png_g_device;
     unsigned char* png_b_device;
@@ -72,7 +73,10 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
     cudaMemcpy(png_g_device, png_g, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
     cudaMemcpy(png_b_device, png_b, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
     cudaMemcpy(png_a_device, png_a, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
-
+    double section2E = CycleTimer::currentSeconds();
+    std::cout << "Section2 time " << section2E-section2S << std::endl;
+    
+    double section3S = CycleTimer::currentSeconds();
     unsigned char* out_r_device;
     unsigned char* out_g_device;
     unsigned char* out_b_device;
@@ -81,6 +85,8 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
     cudaMalloc((void **)&out_g_device, newIm_width*newIm_height*sizeof(unsigned char));
     cudaMalloc((void **)&out_b_device, newIm_width*newIm_height*sizeof(unsigned char));
     cudaMalloc((void **)&out_a_device, newIm_width*newIm_height*sizeof(unsigned char));
+    double section3E = CycleTimer::currentSeconds();
+    std::cout << "Section3 time " << section3E-section3S << std::endl;
     
     double startTime = CycleTimer::currentSeconds();
     kernelWarpPerspective<<<gridDim, blockDim>>>(png_width, png_height, newIm_width, newIm_height,
@@ -88,10 +94,7 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
                                                 png_b_device, png_a_device);
     double endTime = CycleTimer::currentSeconds();
     std::cout << "Actual kernel time " << endTime-startTime << std::endl;
-    cudaFree(png_r_device);
-    cudaFree(png_g_device);
-    cudaFree(png_b_device);
-    cudaFree(png_a_device);
+    
 
     double lMemcpyTime = CycleTimer::currentSeconds();
     cudaMemcpy(newImR, out_r_device, newIm_width*newIm_height*sizeof(unsigned char), cudaMemcpyDeviceToHost); //CHECK ORDER OF ARGS HERE
@@ -100,11 +103,18 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
     cudaMemcpy(newImA, out_a_device, newIm_width*newIm_height*sizeof(unsigned char), cudaMemcpyDeviceToHost);
     double lEndMemcpyTime = CycleTimer::currentSeconds();
     std::cout << "Memcpy kernel time " << lEndMemcpyTime-lMemcpyTime << std::endl;
-
+    
+    double section4S = CycleTimer::currentSeconds();
+    cudaFree(png_r_device);
+    cudaFree(png_g_device);
+    cudaFree(png_b_device);
+    cudaFree(png_a_device);
     cudaFree(out_r_device);
     cudaFree(out_g_device);
     cudaFree(out_b_device);
     cudaFree(out_a_device);
+    double section4E = CycleTimer::currentSeconds();
+    std::cout << "Section4 time " << section4E-section4S << std::endl;
     
     double overallEndTime = CycleTimer::currentSeconds();
     std::cout << "Overall warp persp time " << overallEndTime-overallStartTime << std::endl;
