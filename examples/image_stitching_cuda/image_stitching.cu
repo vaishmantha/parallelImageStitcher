6c734671a32068c450ca6939e864b0a59cb0fdf3
Adithi Rao <adigrao2012@gmail.com>
2021-05-16 18:30:26 -0400

    Fixing correctness

--- examples/image_stitching_cuda/image_stitching.cu
+++ examples/image_stitching_cuda/image_stitching.cu
@@ -71,15 +71,15 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
     unsigned char* png_g_device;
     unsigned char* png_b_device;
     unsigned char* png_a_device;
-    cudaMalloc((void **)&png_r_device, png_height*png_width*sizeof(char));
-    cudaMalloc((void **)&png_g_device, png_height*png_width*sizeof(char));
-    cudaMalloc((void **)&png_b_device, png_height*png_width*sizeof(char));
-    cudaMalloc((void **)&png_a_device, png_height*png_width*sizeof(char));
+    cudaMalloc((void **)&png_r_device, png_height*png_width*sizeof(unsigned char));
+    cudaMalloc((void **)&png_g_device, png_height*png_width*sizeof(unsigned char));
+    cudaMalloc((void **)&png_b_device, png_height*png_width*sizeof(unsigned char));
+    cudaMalloc((void **)&png_a_device, png_height*png_width*sizeof(unsigned char));
 
-    cudaMemcpy(png_r_device, png_r, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(png_g_device, png_g, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(png_b_device, png_b, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(png_a_device, png_a, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(png_r_device, png_r, png_height*png_width*sizeof(unsigned char), cudaMemcpyHostToDevice);
+    cudaMemcpy(png_g_device, png_g, png_height*png_width*sizeof(unsigned char), cudaMemcpyHostToDevice);
+    cudaMemcpy(png_b_device, png_b, png_height*png_width*sizeof(unsigned char), cudaMemcpyHostToDevice);
+    cudaMemcpy(png_a_device, png_a, png_height*png_width*sizeof(unsigned char), cudaMemcpyHostToDevice);
     double section2E = CycleTimer::currentSeconds();
     std::cout << "Section2 time " << section2E-section2S << std::endl;
     
