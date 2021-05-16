4289d39bb08220e8a3df214c9168f06888581673
Adithi Rao <adigrao2012@gmail.com>
2021-05-16 17:14:28 -0400

    Timing cuda

--- examples/image_stitching_cuda/image_stitching.cu
+++ examples/image_stitching_cuda/image_stitching.cu
@@ -63,15 +63,15 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
     unsigned char* png_g_device;
     unsigned char* png_b_device;
     unsigned char* png_a_device;
-    cudaMalloc((void **)&png_r_device, png_height*png_width*sizeof(char));
-    cudaMalloc((void **)&png_g_device, png_height*png_width*sizeof(char));
-    cudaMalloc((void **)&png_b_device, png_height*png_width*sizeof(char));
-    cudaMalloc((void **)&png_a_device, png_height*png_width*sizeof(char));
+    // cudaMalloc((void **)&png_r_device, png_height*png_width*sizeof(char));
+    // cudaMalloc((void **)&png_g_device, png_height*png_width*sizeof(char));
+    // cudaMalloc((void **)&png_b_device, png_height*png_width*sizeof(char));
+    // cudaMalloc((void **)&png_a_device, png_height*png_width*sizeof(char));
 
-    cudaMemcpy(png_r_device, png_r, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(png_g_device, png_g, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(png_b_device, png_b, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(png_a_device, png_a, png_height*png_width*sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(png_r_device, png_r, png_height*png_width*sizeof(char));
+    cudaMemcpyToSymbol(png_g_device, png_g, png_height*png_width*sizeof(char));
+    cudaMemcpyToSymbol(png_b_device, png_b, png_height*png_width*sizeof(char));
+    cudaMemcpyToSymbol(png_a_device, png_a, png_height*png_width*sizeof(char));
 
     unsigned char* out_r_device;
     unsigned char* out_g_device;
@@ -88,12 +88,12 @@ void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* 
                                                 png_b_device, png_a_device);
     double endTime = CycleTimer::currentSeconds();
     std::cout << "Actual kernel time " << endTime-startTime << std::endl;
-    cudaFree(png_r_device);
-    cudaFree(png_g_device);
-    cudaFree(png_b_device);
-    cudaFree(png_a_device);
+    // cudaFree(png_r_device);
+    // cudaFree(png_g_device);
+    // cudaFree(png_b_device);
+    // cudaFree(png_a_device);
 
-    //May not have to malloc here
+    
     cudaMemcpy(newImR, out_r_device, newIm_width*newIm_height*sizeof(unsigned char), cudaMemcpyDeviceToHost); //CHECK ORDER OF ARGS HERE
     cudaMemcpy(newImG, out_g_device, newIm_width*newIm_height*sizeof(unsigned char), cudaMemcpyDeviceToHost);
     cudaMemcpy(newImB, out_b_device, newIm_width*newIm_height*sizeof(unsigned char), cudaMemcpyDeviceToHost);
