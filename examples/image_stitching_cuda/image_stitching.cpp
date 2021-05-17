334ca54baf233fb45571b8b82a69f0de8b345db9
Adithi Rao <adigrao2012@gmail.com>
2021-05-17 00:57:12 -0400

    Revert

--- examples/image_stitching_cuda/image_stitching.cpp
+++ examples/image_stitching_cuda/image_stitching.cpp
@@ -179,27 +179,28 @@ MatrixXd computeRansac(std::list<ezsift::MatchPair> match_li){
         // double prodStart = CycleTimer::currentSeconds();
         MatrixXd prod = H * homogeneous_loc2.transpose();
         double diff;
-        // bool divide_by_zero = false;
+        bool divide_by_zero = false;
         
         // double insideLoopStart = CycleTimer::currentSeconds();
-        ransacIterationDiff(prod, locs1, threshold, &count);
-        // for(int i = 0; i < prod.cols(); i++){ //FIX: 100s of cols here, so cudify
-        //     if(prod.transpose()(i, 2) == 0){
-        //         divide_by_zero = true;
-        //     }
-        //     if(!divide_by_zero){
-        //         diff = (Matslice(prod.transpose(), i, 0, 1, 2)/prod.transpose()(i, 2) - Matslice(locs1, i, 0, 1, locs1.cols())).norm(); 
-        //         if(diff < threshold){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // double insideLoopEnd = CycleTimer::currentSeconds();
+        // ransacIterationDiff(prod, locs1, threshold, &count);
+        for(int i = 0; i < prod.cols(); i++){ //FIX: 100s of cols here, so cudify
+            if(prod.transpose()(i, 2) == 0){
+                divide_by_zero = true;
+            }
+            if(!divide_by_zero){
+                diff = sqrt(pow((prod(i,0)/prod.transpose()(i, 2) - locs1(0,i)),2) + pow((prod(i,1)/prod.transpose()(i, 2) - locs1(1,i)), 2)); 
+                // diff = (Matslice(prod.transpose(), i, 0, 1, 2)/prod.transpose()(i, 2) - Matslice(locs1, i, 0, 1, locs1.cols())).norm(); 
+                if(diff < threshold){
+                    count++;
+                }
+            }
+        }
+        double insideLoopEnd = CycleTimer::currentSeconds();
         // std::cout << "Inside loop time: " << insideLoopEnd - insideLoopStart << std::endl;
 
-        // if (!divide_by_zero){
-        count_list[it] = count;
-        // }      
+        if (!divide_by_zero){
+            count_list[it] = count;
+        }      
     }
 
     // double loopEnd = CycleTimer::currentSeconds();
