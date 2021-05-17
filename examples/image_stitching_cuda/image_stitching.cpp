687cf421ce9a834e8fe66e3a6f686bcc890d8631
Adithi Rao <adigrao2012@gmail.com>
2021-05-17 02:48:27 -0400

    Ransac change

--- examples/image_stitching_cuda/image_stitching.cpp
+++ examples/image_stitching_cuda/image_stitching.cpp
@@ -14,9 +14,9 @@ using Eigen::MatrixXd;
 void dummyWarmup();
 void ransacIterationDiff(MatrixXd prod, MatrixXd locs1, int threshold, int* count);
 // void placeImage(MatrixXd newImage, MatrixXd* resImg, double min_x, double min_y, double max_x, double max_y);
-// void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* png_b, unsigned char* png_a, 
-//         int png_width, int png_height, unsigned char* newImR, unsigned char* newImG, unsigned char* newImB, unsigned char* newImA, 
-//         MatrixXd H, int curr_width, int curr_height);
+void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* png_b, unsigned char* png_a, 
+        int png_width, int png_height, unsigned char* newImR, unsigned char* newImG, unsigned char* newImB, unsigned char* newImA, 
+        MatrixXd H, int curr_width, int curr_height);
 
 MatrixXd Matslice(MatrixXd array, int start_row, int start_col, int height, int width){
     MatrixXd sl = MatrixXd::Constant(height, width, 0);
@@ -84,28 +84,28 @@ MatrixXd computeHomography(MatrixXd x1, MatrixXd x2){
     return finalH.transpose();
 }
 
-void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* png_b, unsigned char* png_a, 
-        int png_width, int png_height, unsigned char* newImR, unsigned char* newImG, unsigned char* newImB, unsigned char* newImA, 
-        MatrixXd H, int curr_width, int curr_height){
-    int i; 
-    for(i=0; i< png_height; i++){ 
-        for(int j=0; j<png_width; j++){
-            MatrixXd tmp = MatrixXd::Constant(3,1, 0.0);
-            tmp(0,0) = j;
-            tmp(1,0) = i;
-            tmp(2,0) = 1;
-            MatrixXd res = H*tmp;
-            MatrixXd tm =  Matslice(res, 2, 0, 1, res.cols()).replicate(3,1); //(MatrixXd array, int start_row, int start_col, int height, int width)
-            res = res.cwiseQuotient(tm);
-            if ((int)res(0,0) >= 0 && (int)res(0,0) < curr_width && (int)res(1,0) >= 0 && (int)res(1,0) < curr_height){
-                newImR[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_r[i*png_width + j];
-                newImG[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_g[i*png_width + j];
-                newImB[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_b[i*png_width + j];
-                newImA[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_a[i*png_width + j];
-            }
-        }
-    }
-}
+// void warpPerspective(unsigned char* png_r, unsigned char* png_g, unsigned char* png_b, unsigned char* png_a, 
+//         int png_width, int png_height, unsigned char* newImR, unsigned char* newImG, unsigned char* newImB, unsigned char* newImA, 
+//         MatrixXd H, int curr_width, int curr_height){
+//     int i; 
+//     for(i=0; i< png_height; i++){ 
+//         for(int j=0; j<png_width; j++){
+//             MatrixXd tmp = MatrixXd::Constant(3,1, 0.0);
+//             tmp(0,0) = j;
+//             tmp(1,0) = i;
+//             tmp(2,0) = 1;
+//             MatrixXd res = H*tmp;
+//             MatrixXd tm =  Matslice(res, 2, 0, 1, res.cols()).replicate(3,1); //(MatrixXd array, int start_row, int start_col, int height, int width)
+//             res = res.cwiseQuotient(tm);
+//             if ((int)res(0,0) >= 0 && (int)res(0,0) < curr_width && (int)res(1,0) >= 0 && (int)res(1,0) < curr_height){
+//                 newImR[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_r[i*png_width + j];
+//                 newImG[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_g[i*png_width + j];
+//                 newImB[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_b[i*png_width + j];
+//                 newImA[((int)res(1,0))*curr_width +  (int)res(0,0)] = (int)png_a[i*png_width + j];
+//             }
+//         }
+//     }
+// }
 
 MatrixXd computeNormalizedHomography(MatrixXd x1, MatrixXd x2, 
                                     MatrixXd homogeneous_x1, 
@@ -210,8 +210,8 @@ MatrixXd computeRansac(std::list<ezsift::MatchPair> match_li){
                 divide_by_zero = true;
             }
             if(!divide_by_zero){
-                diff = sqrt(pow((prod(0,i)/prod(2, i) - locs1(i,0)),2) + pow((prod(1,i)/prod(2,i) - locs1(i,1)), 2)); 
-                // diff = (Matslice(prod.transpose(), i, 0, 1, 2)/prod.transpose()(i, 2) - Matslice(locs1, i, 0, 1, locs1.cols())).norm(); 
+                // diff = sqrt(pow((prod(0,i)/prod(2, i) - locs1(i,0)),2) + pow((prod(1,i)/prod(2,i) - locs1(i,1)), 2)); 
+                diff = (Matslice(prod.transpose(), i, 0, 1, 2)/prod.transpose()(i, 2) - Matslice(locs1, i, 0, 1, locs1.cols())).norm(); 
                 if(diff < threshold){
                     count++;
                 }
